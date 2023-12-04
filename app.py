51f1c97fab1c44195e6cb8ab1c5ec4057d6ddf6e
SC-V <37342773+SC-V@users.noreply.github.com>
2023-12-04 14:21:45 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,113 @@
+import random
+import streamlit as st
+from time import sleep
+
+
+SPIN_BONUS_LIST = [
+    "Lamborgini Diablo 1990 (1:16 model)",
+    "One free ride",
+    "Meshok kartoshki",
+    "Nintendo Switch",
+    "Additional 5% discount for 10 rides",
+    "Italian pizza voucher for Pizzeria Balboa",
+    "Amazon $20 gift card",
+    "Amazon $10 gift card",
+    "Cart of bananas",
+    "Andrey Kudryashov's unsolicited advice",
+    "1 free night at Sofitel presidential suite",
+    "1 free AirBnB experience ticket",
+    "One way ticket to Pakistan",
+    "Jar of strawberry jam",
+    "Branded Yango hoodie",
+    "2 hours of karting for your and 2 friends"
+]
+
+
+def chat_answer(prompt: str, subscription_enabled: bool):
+    if subscription_enabled:
+        with st.chat_message(name="User",
+                             avatar="🧑‍💻"):
+            st.write(prompt)
+
+        with st.chat_message(name="Master of support",
+                             avatar="🧑‍💻"):
+            st.write("I'll answer straight away!")
+            sleep(2)
+            st.write("How may I help you?")
+    else:
+        with st.chat_message(name="User",
+                             avatar="🧑‍💻"):
+            st.write(prompt)
+
+        with st.chat_message(name="Support bot",
+                             avatar="🤖"):
+            st.write("I'll answer in 100 light years")
+
+
+st.set_page_config(page_title=f"Yango Hackaton Dec 2023",
+                   layout="wide")
+
+st.markdown(f"# Yango Turbo")
+st.markdown("This is an app to showcase the **:red[Yango Turbo]** subscription for the 1st Yango Hackaton Dec 2023.")
+
+subscription_enabled = st.checkbox(label="Yango Turbo",
+                                   value=False,
+                                   help="Click to simulate the purchase of Yango Turbo")
+
+subscription_discount = 0.1
+
+econom_price = 10.0
+comfort_price = 14.0
+express_price = 9.0
+
+if subscription_enabled:
+    col_lets_roll, col_result = st.columns(2)
+
+    with col_lets_roll:
+        if st.button("Roll your bonus benefit!"):
+            with col_result:
+                with st.status("Ready to spin!", expanded=True) as status:
+                    st.write("Spinning the wheel...")
+                    sleep(1)
+                    st.write("Getting the best bonuses...")
+                    sleep(1)
+                    st.write("You are gonna be amazed...")
+                    sleep(1)
+                    prize_id = random.randint(0, len(SPIN_BONUS_LIST) - 1)
+                    status.update(label=f"Wow! You won **{SPIN_BONUS_LIST[prize_id]}**", state="complete", expanded=False)
+
+
+col_econom, col_comfort, col_express = st.columns(3)
+
+with col_econom:
+    if subscription_enabled:
+        st.metric(label=r"Econom 🏎",
+                  value=f"{econom_price * (1 - subscription_discount)}/S",
+                  delta=f"{subscription_discount * 100}% off")
+    else:
+        st.metric(label="Econom",
+                  value=f"{econom_price}/S")
+
+with col_comfort:
+    if subscription_enabled:
+        st.metric(label=r"Comfort 🏎",
+                  value=f"{comfort_price * (1 - subscription_discount)}/S",
+                  delta=f"{subscription_discount * 100}% off")
+    else:
+        st.metric(label="Comfort",
+                  value=f"{comfort_price}/S")
+
+with col_express:
+    if subscription_enabled:
+        st.metric(label=r"Express 🏎",
+                  value=f"{express_price * (1 - subscription_discount)}/S",
+                  delta=f"{subscription_discount * 100}% off")
+    else:
+        st.metric(label="Express",
+                  value=f"{express_price}/S")
+
+
+prompt = st.chat_input(placeholder="Operator will answer shortly" if subscription_enabled else "Your message will be processed by a bot")
+
+if prompt:
+    chat_answer(prompt, subscription_enabled)
